49773afc0f88891d7c6c0b551fd3b197e7f33502
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-05-30 13:31:08 +0100

    Fix broken syntax.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'django-form-models',
     packages = find_packages(),
     include_package_data=True,
-    install_requires=['django-orderable>=1.0.1']
+    install_requires=['django-orderable>=1.0.1'],
     version = '0.1',
     description = 'Sample models for modelling django forms and fields, and integrate with crispy forms layout.',
     author = 'Incuna Ltd',
