8f41835474596e33456736896b80b4f5585b8893
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-05-30 13:30:35 +0100

    Nuke migrations, add version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages = find_packages(),
     include_package_data=True,
     install_requires=['django-orderable>=1.0.1']
-    version = get_version(),
+    version = '0.1',
     description = 'Sample models for modelling django forms and fields, and integrate with crispy forms layout.',
     author = 'Incuna Ltd',
     author_email = 'admin@incuna.com',
